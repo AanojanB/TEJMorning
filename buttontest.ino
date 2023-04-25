20217dddef2b854dd0f929c8d045cd256ede30ab
Henry-Zelonka <131679900+Henry-Zelonka@users.noreply.github.com>
2023-04-25 09:18:56 -0400

    Button Test Code
    This code tests the button and outputs a 1 if the button is pressed and a 0 if it is not pressed.

---
+++ buttontest.ino
@@ -0,0 +1,30 @@
+int red = 10;
+int yellow =   9;
+int green = 8 ;
+int button = 12;
+
+
+
+void setup() {
+
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+
+  pinMode(button, INPUT);
+
+  digitalWrite(green,HIGH);
+
+  Serial.begin(9600);
+  
+
+}
+
+void loop(){
+  
+ Serial.println(digitalRead(button));
+
+}
+    
+
+  
